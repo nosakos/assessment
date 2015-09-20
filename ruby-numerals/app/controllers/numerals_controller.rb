ac35791b9a1459a6902d8c0f11b9b13a88ca58bf
Ákos <nos.akos@gmail.com>
2015-09-20 23:38:34 +0200

    form

--- ruby-numerals/app/controllers/numerals_controller.rb
+++ ruby-numerals/app/controllers/numerals_controller.rb
@@ -1,8 +1,19 @@
 class NumeralsController < ApplicationController
 
 	def index
+		render :index
 	end
 
 	def numerals
+		render plain: ((is_number?(params[:number])) ? to_string(params[:number]) : "Please enter the number in the text box!")
 	end
+
+	private
+		def is_number? string
+  			true if Integer(string) rescue false
+  		end
+
+  		def to_string number
+  			""
+  		end
 end
